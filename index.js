aceddb5465eedc428ff231eebcf9c87deea456c7
frontendella <ryukhinaella@gmail.com>
2023-07-02 11:14:49 -0700

    Refactor to use dependency I - env variable

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const apiKey = "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"
+import { process } from '/env'
 
-const url = "https://api.openai.com/v1/completions" 
 
-fetch(url, {
+const setupTextarea = document.getElementById('setup-textarea') 
+const setupInputContainer = document.getElementById('setup-input-container')
+const movieBossText = document.getElementById('movie-boss-text')
+
+const apiKey = process.env.OPENAI_API_KEY
+// const apiKey = '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'
+const url = 'https://api.openai.com/v1/completions'
+
+document.getElementById("send-btn").addEventListener("click", () => {
+  // if (setupTextarea.value) {
+    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
+  // }
+  fetchBotReply()
+})
+
+function fetchBotReply(){
+  fetch(url, {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
     },
     body: JSON.stringify({
-        'model': 'text-davinci-003',
-        'prompt': 'Sound sympathetic in five words or less.'
+      'model': 'text-davinci-003',
+      'prompt': 'Sound enthusiastic in five words or less.' 
     })
-}).then(response => response.json()).then(data => console.log(data))
+  }).then(response => response.json()).then(data => 
+  movieBossText.innerText = data.choices[0].text
+  )
+}
 
-//›{id: "cmpl-78r0C25HIzI0VryLgb8HXiBr3Xm8v", object: "text_completion", created: 1682344720, model: "text-davinci-003", choices: [{text: " I'm here for you.", index: 0, logprobs: null, finish_reason: "stop"}], usage: {prompt_tokens: 8, completion_tokens: 8, total_tokens: 16}}
